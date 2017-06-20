ecab020a306c3ff74e2315589fb25837e87cddb1
abigesh <abigesh@gmail.com>
2017-06-20 17:56:06 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+ 
+int main()
+{
+    int number;
+    printf("enter the number: ");
+    scanf("%d", &number);
+    printf("The number is %s", (n%2==0 ? "EVEN" : "ODD"));
+    return 0;
+}
